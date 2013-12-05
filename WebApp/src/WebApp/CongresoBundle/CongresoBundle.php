afe7b19ee7ae4d040df3e40f993eb74bbe5f87bc
Isabel Aguilar Augustin <tichatuch@gmail.com>
2013-12-05 15:58:14 +0100

    Borrados ficheros innecesarios

--- WebApp/src/WebApp/CongresoBundle/CongresoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WebApp\CongresoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CongresoBundle extends Bundle
-{
-}
--- WebApp/src/WebApp/UsuarioBundle/UsuarioBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WebApp\UsuarioBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class UsuarioBundle extends Bundle
-{
-}
